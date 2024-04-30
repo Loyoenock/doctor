bdf6ab730bb5ab4a478f93be15a180e971f967bd
Loyoenock <loyoenock@gmail.com>
2024-04-30 09:52:10 +0300

    Skeleton effect implemented

--- doctor-booking/app/_components/CategorySearch.jsx
+++ doctor-booking/app/_components/CategorySearch.jsx
@@ -34,13 +34,22 @@ function CategorySearch() {
     </div>
       {/* Display Category List */}
       <div className='grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 mt-5'>        
-        {categoryList.map((item, index) =>index<6&&    (
+        {categoryList.length>0?categoryList.map((item, index) =>index<6&&    (
           <div key={index} className='flex flex-col text-center gap-2 items-center p-5 bg-blue-50 m-2 rounded-lg hover:scale-110 transition-all ease-in-out cursor-pointer'>
             <img src={item.attributes?.Icon?.data.attributes?.url} alt='Icon'
               width={40} height={40} />
             <label className='text-blue-600 text-sm'>{item.attributes?.Name}</label>
           </div>
-        ))}
+        ))
+        :
+        [1,2,3,4,5,6].map((index, item)=>(
+          <div className='bg-slate-200 m-2 
+        w-[100px] h-[100px] animate-pulse rounded-lg
+        '>
+        </div>
+        ))
+       
+        }
       </div>
     </div>
   )
